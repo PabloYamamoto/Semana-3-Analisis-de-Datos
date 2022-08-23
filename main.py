5696fb3c1730dee21bbac1a0ceb744667b532a43
PabloYamamoto <pablo@MacBook-Pro-29.local>
2022-08-23 15:58:05 -0500

    Adding excel files to the gitignore and structuring the main.py file

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+--Viajes--
+Preguntas h, j, t
+"""
+def viajes():
+    print("Viajes")
+
+
+
+"""
+--Ciudades--
+Preguntas o-s, f
+"""
+def ciudades():
+    print("Ciudades")
+
+
+
+"""
+--Shops--
+Preguntas l - s
+"""
+def tiendas():
+    print("Tiendas")
+
+
+
+
+"""--Vehiculos-- 
+Preguntas a - e, g, i, k
+"""
+def vehiculos():
+    print("Vehiculos")
+
+
+
+
+def main():
+    print("Hello World")
+
+main()
