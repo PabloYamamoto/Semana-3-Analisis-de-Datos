f717592d75e77d11396b2a0ea320e31db6861d01
PabloYamamoto <pablo@MacBook-Pro-29.local>
2022-08-23 18:31:41 -0500

    Adding notes file and structure to our python

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 """
 --Viajes--
 Preguntas h, j, t
 """
 def viajes():
     print("Viajes")
-
 
 
 """
@@ -24,16 +26,20 @@ def tiendas():
     print("Tiendas")
 
 
-
-
-"""--Vehiculos-- 
+"""
+--Vehiculos-- 
 Preguntas a - e, g, i, k
 """
 def vehiculos():
     print("Vehiculos")
 
 
+def preprocess(df):
 
+    # Remove unamed columns
+    df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+
+    
 
 def main():
     print("Hello World")
